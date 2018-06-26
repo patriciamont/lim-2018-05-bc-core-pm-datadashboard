fd37e1502c2fa69c46241ce447c3fbaec661dd9f
patriciamont <fiorellamonteverde@gmail.com>
2018-06-26 12:10:23 -0500

    probando metodos para agrupar cohorts conforme a su sede

--- src/main.js
+++ src/main.js
@@ -1,53 +1,82 @@
-let lista =document.getElementById('main');
-let elementMain= lista;
+let lista = document.getElementById('main');
+let elementMain = lista;
 
 let dataCohort = () => {
-   fetch('http://127.0.0.1:5500/data/cohorts.json')
-       .then(response => {
-           return response.json();
-       })
-       .then(json => {
+    fetch('http://127.0.0.1:5500/data/cohorts.json')
+        .then(response => {
+            return response.json();
+        })
+        .then(json => {
 
-           const cohort = json;
+            const cohort = json;
 
-           for (let i = 0; i < cohort.length; i++) {
+            for (let i = 0; i < cohort.length; i++) {
 
-               const cohortContent = document.createElement('div');
-               const paragraphContent = document.createElement('div');
-               const buttonContent = document.createElement('div');
-               const paragraph = document.createElement('p');
-               const button = document.createElement('button');
-               const paragraphText = document.createTextNode(cohort[i].id);
-               const buttonText = document.createTextNode('Entrar')
-               button.dataset.cohort = cohort[i].id;
-               paragraph.appendChild(paragraphText);
-               paragraphContent.appendChild(paragraph);
-               cohortContent.appendChild(paragraphContent);
+                const cohortContent = document.createElement('div');
+                const paragraphContent = document.createElement('div');
+                const buttonContent = document.createElement('div');
+                const paragraph = document.createElement('p');
+                const button = document.createElement('button');
+                const paragraphText = document.createTextNode(cohort[i].id);
+                const buttonText = document.createTextNode('Entrar')
+                button.dataset.cohort = cohort[i].id;
+                paragraph.appendChild(paragraphText);
+                paragraphContent.appendChild(paragraph);
+                cohortContent.appendChild(paragraphContent);
 
-               button.appendChild(buttonText);
-               buttonContent.appendChild(button);
-               cohortContent.appendChild(buttonContent);
+                button.appendChild(buttonText);
+                buttonContent.appendChild(button);
+                cohortContent.appendChild(buttonContent);
 
-               elementMain.appendChild(cohortContent);
-               
+                elementMain.appendChild(cohortContent);
 
-           }
-       })
+
+            }
+        })
 
 }
 
-const btnLima = document.getElementById('botonlima');
+let btn = document.getElementById('btn-venues');
+let btnSedes = btn;
+ let btnCohort = () => {
+    fetch('http://127.0.0.1:5500/data/cohorts.json')
+        .then(response => {
+            return response.json();
+        })
+        .then(json => {
+
+            const cohort = json;
+
+            for (let i = 0; i < cohort.length; i++) {
+
+                const boxbtn = document.createElement('div');
+                const btns = document.createElement('button');
+                const btnText = document.createTextNode('click');
+                btns.appendChild(btnText);
+                boxbtn.appendChild(btns);
+                btnSedes.appendChild(boxbtn);
+
+
+
+            }
+        })
+
+}
+
+
+
+/*const btnLima = document.getElementById('botonlima');
 btnLima.addEventListener('click', () => {dataCohort()
     document.getElementById('btn-venues').style = 'display:none';
-    document.getElementById('vacio').style = 'display:block';
+   // document.getElementById('vacio').style = 'display:block';//
 });
+*/
 
-
-elementMain.addEventListener('click', (e)=>{
-  console.log(e.target.dataset.cohort);
-  if (e.target.dataset.cohort === 'lim-2018-03-pre-core-pw'){
-      elementMain.innerHTML = `<div>Hola Mundo ${e.target.dataset.cohort}</div>`;
-  }
+elementMain.addEventListener('click', (e) => {
+    console.log(e.target.dataset.cohort);
+    if (e.target.dataset.cohort === 'lim-2018-03-pre-core-pw') {
+        elementMain.innerHTML = `<div>Hola Mundo ${e.target.dataset.cohort}</div>`;
+    }
 })
 
 
@@ -65,6 +94,18 @@ const datadash = (dd) => {
 
                                     const [cohorts, users, progress] = data;
                                     dd(cohorts, users, progress);
+
+
+                                    const option = {
+                                        cohort: 0,
+                                        cohorData: {
+                                            users: 0,
+                                            progress: 0,
+                                        },
+                                        orderBy: 'asc',
+                                        search: 'desc'
+
+                                    }
                                 });
                         });
                 });
